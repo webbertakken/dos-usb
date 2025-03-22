7062afe30a15c3638cb0a9502a1dfacc74a12396
Webber <webber@takken.io>
2025-03-22 22:29:36 +0100

    chore: remove unnecessary files

--- remove-from-git.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git rm --cached bin/DOSBoxPortable/Data/autoexec.bat
-git rm --cached bin/DOSBoxPortable/Data/mapper-revenge-of-the-mutant-camels.txt
-git rm --cached bin/DOSBoxPortable/Data/settings/DOSBoxPortableSettings.ini
-echo Files removed from Git tracking
